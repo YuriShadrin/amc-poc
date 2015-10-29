09a1b9270aa2134a2b4437b84f8dd843d92ac219
YuriShadrin <yshadrin@exadel.com>
2015-10-29 15:24:37 +0200

    WIKI

---
+++ src/main/java/com/exadel/amc/wikipedia/WikiClient.java
@@ -0,0 +1,14 @@
+package com.exadel.amc.wikipedia;
+
+import org.nextlets.erc.ERCEndpoint;
+import org.nextlets.erc.ERCParam;
+
+import com.exadel.amc.wikipedia.data.ArticleStatistics;
+import com.exadel.amc.wikipedia.data.ResponseDesirializer;
+
+public interface WikiClient {
+
+	@ERCEndpoint(endpoint="/json/en/latest30/{acticle}", deserializer=ResponseDesirializer.class)
+	ArticleStatistics getArticleStatisticsLatest30(@ERCParam(name="acticle") String acticle);
+	
+}
